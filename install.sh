f790f85a5a69a1933d31b17a53df31c11d2924b5
Barry Warnock <brrwarnock@gmail.com>
2018-06-13 13:39:22 -0700

    fix install scripts

--- install.sh
+++ install.sh
@@ -5,23 +5,29 @@ install () {
 	then
 	    if [ -z $(command -v yaourt) ]
 	    then
-		pacman -S yaourt
+		sudo pacman -S yaourt
 	    fi
 	    yaourt -S $1 --noconfirm
 	fi
     fi
 }
 
+install base-devel
+install git
+
+install sbcl
+install slime
+
 . stumpwm/install_stump.sh
 
 install zsh
 if [ -z $(echo $SHELL | grep zsh) ]
 then
-    chsh -s zsh
+    chsh -s /usr/bin/zsh
 fi
 
 install emacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
-install google-chrome-stable
+install google-chrome
 
--- stumpwm/install_stump.sh
+++ stumpwm/install_stump.sh
@@ -6,6 +6,7 @@ else
     curl http://beta.quicklisp.org/quicklisp.lisp > /tmp/quicklisp.lisp
     sbcl --non-interactive --load /tmp/quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql:add-to-init-file)"
     sbcl --non-interactive --eval "(ql:quickload \"stumpwm\")"
+    sbcl --non-interactive --eval "(ql:quickload \"swank\")"
     sudo cp stumpwm.sh /usr/bin/stumpwm.sh
     sudo cp stumpwm.desktop /usr/share/xsessions/stumpwm.desktop 
 fi
